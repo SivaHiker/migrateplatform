c5c0f31eda55bae9268ad7fd10c217343db6c5ed
Siva <siva@Admins-MacBook-Pro.local>
2017-11-15 17:37:22 +0530

    Fixing the issue and debugging the lines

--- main.go
+++ main.go
@@ -40,12 +40,6 @@ func main() {
 	}
 	defer outputfile1.Close()
 
-	outputfile2, err := os.Create("text2.txt")
-	if(err!=nil){
-		fmt.Println("Not able to create a file")
-	}
-	defer outputfile2.Close()
-
 	csvfile1, err := os.Create("result1.csv")
 	if(err!=nil){
 		fmt.Println("Not able to create a csv file")
@@ -54,16 +48,6 @@ func main() {
 	writer1 := csv.NewWriter(csvfile1)
 	defer writer1.Flush()
 	defer csvfile1.Close()
-
-
-	csvfile2, err := os.Create("result2.csv")
-	if(err!=nil){
-		fmt.Println("Not able to create a csv file")
-	}
-
-	writer2 := csv.NewWriter(csvfile2)
-	defer writer2.Flush()
-	defer csvfile2.Close()
 
 	limiter := time.Tick(time.Nanosecond * 2000000)
 
@@ -74,27 +58,20 @@ func main() {
 			break
 		}
 		var user platformUser
-		var userdetails platformUserDetails
-		//fmt.Printf(" > Read %d characters\n", len(line))
-		//fmt.Println(line)
+
 		uservalues := strings.Split(line,"+")
 		uid := uservalues[0]
-
+        fmt.Println(strings.TrimSpace(uid))
 
 		//uid="WcKAaVIchw737usm"
-		fmt.Println("select * from platform_user where  hike_uid=\""+uid+"\"")
+		fmt.Println("select * from platform_user where  hike_uid=\""+strings.TrimSpace(uid)+"\"")
 		<-limiter
-		rows1,err := dbConn.Query("select * from platform_user where  hike_uid=\""+uid+"\"")
+		rows1,err := dbConn.Query("select * from platform_user where  hike_uid=\""+strings.TrimSpace(uid)+"\"")
 		defer rows1.Close()
 		if(err!=nil){
 			fmt.Println("Not able to query the hike uid in the DB -->",uid,err)
 		}
-		fmt.Println("select * from platform_user_details where  hike_uid=\""+uid+"\"")
-		rows2,err := dbConn.Query("select * from platform_user_details where hike_uid=\""+uid+"\"")
-		defer rows2.Close()
-		if(err!=nil){
-			fmt.Println("Not able to query the hike uid in the DB -->",uid,err)
-		}
+
 
 		if rows1.Next() {
 			err := rows1.Scan(&user.ID,&user.HikeUID, &user.PlatformUID, &user.PlatformToken, &user.Msisdn,
@@ -103,19 +80,11 @@ func main() {
 				fmt.Println(err.Error())
 			}
 		}
-		if(rows2.Next()) {
-			err := rows2.Scan(&userdetails.ID,&userdetails.HikeUID, &userdetails.Msisdn, &userdetails.Name,
-				&userdetails.Gender,&userdetails.Circle, &userdetails.CreateTime, &userdetails.UpdateTime)
-			fmt.Println(err)
-		}
-		//userValues ={userd.Token,userd.Msisdn,userd.UID,userd.AppVersion,userd.DeviceKey,userd.DevID,
-		//userd.RegTime,userd.DevToken,userd.DevTokenUpdateTs,userd.DevVersion,userd.DevType,userd.Os,
-		//userd.OsVersion,userd.UpgradeTime,userd.LastActivityTime,userd.AttributeBits,userd.Sound,userd.EndTime,
-		//userd.OriginalAppVersion,userd.Operator,userd.Resolution,userd.Circle,userd.Pdm}
-
-		//outputfile.WriteString(ToString(userd.Token)+"::+"+ToIntegerVal(userd.Msisdn)+"::"+ToString(userd.
-		//	Sound)+"::"+ToIntegerVal(userd.UpgradeTime)+"\n")
-
+		//if(rows2.Next()) {
+		//	err := rows2.Scan(&userdetails.ID,&userdetails.HikeUID, &userdetails.Msisdn, &userdetails.Name,
+		//		&userdetails.Gender,&userdetails.Circle, &userdetails.CreateTime, &userdetails.UpdateTime)
+		//	fmt.Println(err)
+		//}
 
 		userCreateTime := strings.Split(user.CreateTime.String(),"+")
 		userCrTime := userCreateTime[0]
@@ -125,12 +94,12 @@ func main() {
 		userUpTime := userUpdateTime[0]
 
 
-		userDetailCreateTime := strings.Split(userdetails.CreateTime.String(),"+")
-		userDtlCrTime := userDetailCreateTime[0]
-
-
-		userDetailUpdateTime := strings.Split(userdetails.UpdateTime.String(),"+")
-		userDtlUpTime := userDetailUpdateTime[0]
+		//userDetailCreateTime := strings.Split(userdetails.CreateTime.String(),"+")
+		//userDtlCrTime := userDetailCreateTime[0]
+		//
+		//
+		//userDetailUpdateTime := strings.Split(userdetails.UpdateTime.String(),"+")
+		//userDtlUpTime := userDetailUpdateTime[0]
 
 		msisdnReqd := user.Msisdn
 		if strings.HasPrefix(msisdnReqd,"+9") {
@@ -138,21 +107,22 @@ func main() {
 		} else if strings.HasPrefix(msisdnReqd,"+8") {
 			msisdnReqd=strings.Replace(msisdnReqd,"+8","2",1)
 		} else if strings.HasPrefix(msisdnReqd,"+7") {
-			msisdnReqd=strings.Replace(msisdnReqd,"7","3",1)
+			msisdnReqd=strings.Replace(msisdnReqd,"+7","3",1)
 		} else {
 			continue
 		}
 
-		msisdnReqd2 := userdetails.Msisdn
-		if strings.HasPrefix(msisdnReqd,"+9") {
-			msisdnReqd2=strings.Replace(msisdnReqd2,"+9","1",1)
-		} else if strings.HasPrefix(msisdnReqd2,"+8") {
-			msisdnReqd2=strings.Replace(msisdnReqd2,"+8","2",1)
-		} else if strings.HasPrefix(msisdnReqd2,"+7") {
-			msisdnReqd2=strings.Replace(msisdnReqd2,"+7","3",1)
-		} else {
-			continue
-		}
+		//msisdnReqd2 := userdetails.Msisdn
+		//if strings.HasPrefix(msisdnReqd,"+9") {
+		//	msisdnReqd2=strings.Replace(msisdnReqd2,"+9","1",1)
+		//} else if strings.HasPrefix(msisdnReqd2,"+8") {
+		//	msisdnReqd2=strings.Replace(msisdnReqd2,"+8","2",1)
+		//} else if strings.HasPrefix(msisdnReqd2,"+7") {
+		//	msisdnReqd2=strings.Replace(msisdnReqd2,"+7","3",1)
+		//} else {
+		//	continue
+		//}
+
 		count++
 		outputfile1.WriteString(ToIntegerVal(count)+"::"+user.HikeUID+"::"+user.PlatformUID+"::"+user.
 			PlatformToken+"::+"+msisdnReqd+"::"+user.HikeToken+"::"+strings.TrimSpace(userCrTime)+"::"+strings.TrimSpace(userUpTime)+
@@ -163,14 +133,14 @@ func main() {
 				strings.TrimSpace(userCrTime),strings.TrimSpace(userUpTime), ToString(user.Status)},
 		}
 
-		outputfile2.WriteString(ToIntegerVal(count)+"::"+userdetails.HikeUID+"::"+"+"+msisdnReqd+"::"+ToString(userdetails.
-			Name)+"::"+ ToString(userdetails.Gender)+"::"+ToString(userdetails.Circle)+"::"+
-				"::"+strings.TrimSpace(userDtlCrTime)+"::"+strings.TrimSpace(userDtlUpTime)+"\n")
-
-		records2 := [][]string{
-			{ToIntegerVal(count),userdetails.HikeUID,"+"+msisdnReqd2,ToString(userdetails.Name),
-			ToString(userdetails.Gender),ToString(userdetails.Circle), strings.TrimSpace(userDtlCrTime),strings.TrimSpace(userDtlUpTime)},
-			}
+		//outputfile2.WriteString(ToIntegerVal(count)+"::"+userdetails.HikeUID+"::"+"+"+msisdnReqd+"::"+ToString(userdetails.
+		//	Name)+"::"+ ToString(userdetails.Gender)+"::"+ToString(userdetails.Circle)+"::"+
+		//		"::"+strings.TrimSpace(userDtlCrTime)+"::"+strings.TrimSpace(userDtlUpTime)+"\n")
+		//
+		//records2 := [][]string{
+		//	{ToIntegerVal(count),userdetails.HikeUID,"+"+msisdnReqd2,ToString(userdetails.Name),
+		//	ToString(userdetails.Gender),ToString(userdetails.Circle), strings.TrimSpace(userDtlCrTime),strings.TrimSpace(userDtlUpTime)},
+		//	}
 
 		for _, value := range records1 {
 			err := writer1.Write(value)
@@ -180,13 +150,13 @@ func main() {
 			}
 		}
 
-		for _, value := range records2 {
-			err := writer2.Write(value)
-			if(err!=nil){
-				fmt.Println(err.Error())
-				fmt.Println("Not able to write the records into csv file")
-			}
-		}
+		//for _, value := range records2 {
+		//	err := writer2.Write(value)
+		//	if(err!=nil){
+		//		fmt.Println(err.Error())
+		//		fmt.Println("Not able to write the records into csv file")
+		//	}
+		//}
 
 	}
 

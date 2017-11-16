0eac261a481c4e85aa21c1496ff38443ec19f148
Siva <siva@Admins-MacBook-Pro.local>
2017-11-17 01:24:27 +0530

    optimized the script

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 	"bufio"
 	"encoding/csv"
 	"strings"
-	"io"
 	"database/sql"
 )
 
 func main() {
 	var count1 int64
+	var recordsCount int64
 	file, err := os.Open("/home/siva/LatestAppOpenUsers_20170512_to_20171107.txt")
 	defer file.Close()
 
@@ -23,8 +23,7 @@ func main() {
 	}
 
 	dbConn := getDBConnection()
-	dbConn.SetMaxOpenConns(10000)
-
+	dbConn.SetMaxOpenConns(10)
 	defer dbConn.Close()
 	err = dbConn.Ping()
 	if err != nil {
@@ -48,22 +47,35 @@ func main() {
 	defer writer1.Flush()
 	defer csvfile1.Close()
 
-	limiter := time.Tick(time.Nanosecond * 1000000)
+	limiter := time.Tick(time.Nanosecond * 333333333)
 
 	var line string
+	var linecount int
 	for {
-		line, err = reader.ReadString('\n')
-		if err != nil {
-			break
-		}
+		linecount =0
 		var user platformUser
-		uid :=line[0:16]
-		//uid="WcKAaVIchw737usm"
-		fmt.Println("select * from platform_user where  hike_uid=\""+strings.TrimSpace(uid)+"\"")
+		lines := make([]string, 2000)
+		query := ""
+		for linecount<1000 {
+			line, err = reader.ReadString('\n')
+			if err != nil {
+				break
+			}
+			uid :=line[0:16]
+			lines = append(lines,uid)
+			if linecount == 0 {
+				query = query + "\"" + strings.TrimSpace(uid) + "\""
+			} else {
+				query = query + ",\"" + strings.TrimSpace(uid) + "\""
+			}
+			linecount++
+		}
+
+		fmt.Println("select * from platform_user where  uid in ("+query+")")
 		<-limiter
-		rows1,err := dbConn.Query("select * from platform_user where  hike_uid=\""+strings.TrimSpace(uid)+"\"")
+		rows1,err := dbConn.Query("select * from platform_user where  uid in ("+query+")")
 		if(err!=nil){
-			fmt.Println("Not able to query the hike uid in the DB -->",uid,err)
+			fmt.Println(err)
 		}
 
 		if rows1.Next() {
@@ -72,51 +84,53 @@ func main() {
 			if(err!=nil) {
 				fmt.Println(err.Error())
 			}
+			userCreateTime := strings.Split(user.CreateTime.String(),"+")
+			userCrTime := userCreateTime[0]
+
+
+			userUpdateTime := strings.Split(user.UpdateTs.String(),"+")
+			userUpTime := userUpdateTime[0]
+
+
+			msisdnReqd := user.Msisdn
+			if strings.HasPrefix(msisdnReqd,"+9") {
+				msisdnReqd=strings.Replace(msisdnReqd,"+9","1",1)
+			} else if strings.HasPrefix(msisdnReqd,"+8") {
+				msisdnReqd=strings.Replace(msisdnReqd,"+8","2",1)
+			} else if strings.HasPrefix(msisdnReqd,"+7") {
+				msisdnReqd=strings.Replace(msisdnReqd,"+7","3",1)
+			} else {
+				continue
+			}
+
+			count1++
+			recordsCount++
+			outputfile1.WriteString(ToIntegerVal(count1)+"::"+user.HikeUID+"::"+user.PlatformUID+"::"+user.
+				PlatformToken+"::+"+msisdnReqd+"::"+user.HikeToken+"::"+strings.TrimSpace(userCrTime)+"::"+strings.TrimSpace(userUpTime)+
+				"::"+ToString(user.Status)+"\n")
+
+			records1 := [][]string{
+				{ToIntegerVal(count1),user.HikeUID,user.PlatformUID,user.PlatformToken,"+"+msisdnReqd,user.HikeToken,
+					strings.TrimSpace(userCrTime),strings.TrimSpace(userUpTime), ToString(user.Status)},
+			}
+
+			for _, value := range records1 {
+				err := writer1.Write(value)
+				if(err!=nil){
+					fmt.Println(err.Error())
+					fmt.Println("Not able to write the records into csv file")
+				}
+			}
 		}
 		rows1.Close()
 
-		userCreateTime := strings.Split(user.CreateTime.String(),"+")
-		userCrTime := userCreateTime[0]
-
-
-		userUpdateTime := strings.Split(user.UpdateTs.String(),"+")
-		userUpTime := userUpdateTime[0]
-
-
-		msisdnReqd := user.Msisdn
-		fmt.Println(msisdnReqd)
-		if strings.HasPrefix(msisdnReqd,"+9") {
-			msisdnReqd=strings.Replace(msisdnReqd,"+9","1",1)
-		} else if strings.HasPrefix(msisdnReqd,"+8") {
-			msisdnReqd=strings.Replace(msisdnReqd,"+8","2",1)
-		} else if strings.HasPrefix(msisdnReqd,"+7") {
-			msisdnReqd=strings.Replace(msisdnReqd,"+7","3",1)
-		} else {
-			continue
-		}
-
-		count1++
-		outputfile1.WriteString(ToIntegerVal(count1)+"::"+user.HikeUID+"::"+user.PlatformUID+"::"+user.
-			PlatformToken+"::+"+msisdnReqd+"::"+user.HikeToken+"::"+strings.TrimSpace(userCrTime)+"::"+strings.TrimSpace(userUpTime)+
-			"::"+ToString(user.Status)+"\n")
-
-		records1 := [][]string{
-			{ToIntegerVal(count1),user.HikeUID,user.PlatformUID,user.PlatformToken,"+"+msisdnReqd,user.HikeToken,
-				strings.TrimSpace(userCrTime),strings.TrimSpace(userUpTime), ToString(user.Status)},
-		}
-
-		for _, value := range records1 {
-			err := writer1.Write(value)
-			if(err!=nil){
-				fmt.Println(err.Error())
-				fmt.Println("Not able to write the records into csv file")
-			}
-		}
 	}
 
-	if err != io.EOF {
-		fmt.Printf(" > Failed!: %v\n", err)
-	}
+	fmt.Println("Number of records exported from the DB",recordsCount)
+
+	//if err != io.EOF {
+	//	fmt.Printf(" > Failed!: %v\n", err)
+	//}
 
 }
 
